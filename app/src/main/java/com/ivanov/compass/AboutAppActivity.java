ff65739dac7b51af9a71ade51f73639b7a18de90
vasia18 <studiovasia18@gmail.com>
2020-08-14 03:28:32 +0300

    Создано activity_about_author.xml

---
+++ app/src/main/java/com/ivanov/compass/AboutAppActivity.java
@@ -0,0 +1,12 @@
+package com.ivanov.compass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class AboutAppActivity extends AppCompatActivity {
+	@Override
+	public void setContentView(View view) {
+		super.setContentView(view);
+		setContentView(R.layout.activity_about_app);
+	}
+}
--- app/src/main/java/com/ivanov/compass/CompassActivityOne.java
+++ app/src/main/java/com/ivanov/compass/CompassActivityOne.java
@@ -10,129 +10,131 @@ import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class CompassActivityOne extends AppCompatActivity {
 
-    private static final String TAG = "CompassActivity";
+	private static final String TAG = "CompassActivity";
 
-    private Compass compass;
-    private ImageView arrowView;
-    private TextView sotwLabel;  // SOTW is for "side of the world"
+	private Compass compass;
+	private ImageView arrowView;
+	private TextView sotwLabel;  // SOTW is for "side of the world"
 
-    private float currentAzimuth;
-    private SOTWFormatter sotwFormatter;
+	private float currentAzimuth;
+	private SOTWFormatter sotwFormatter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_compass);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_compass);
 
-        sotwFormatter = new SOTWFormatter(this);
+		sotwFormatter = new SOTWFormatter(this);
 
-        arrowView = findViewById(R.id.main_image_hands);
-        sotwLabel = findViewById(R.id.sotw_label);
-        setupCompass();
-    }
+		arrowView = findViewById(R.id.main_image_hands);
+		sotwLabel = findViewById(R.id.sotw_label);
+		setupCompass();
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_compass,menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_compass, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
 
-        int id = item.getItemId();
+		int id = item.getItemId();
 
-        switch (id) {
-            case R.id.about_author:
-                Intent intent = new Intent(this,AboutAuthorActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.action_settings:
-                Intent intent1= new Intent(this, ActionSettingsActivity.class);
-                startActivity(intent1);
-                break;
-            case R.id.theme_activity:
-                Intent intent2=new Intent(this, ThemeActivity.class);
-                startActivity(intent2);
-                break;
-            default:
+		switch (id) {
+			case R.id.menu_about_app:
+				Intent intent = new Intent(this, AboutAppActivity.class);
+				startActivity(intent);
+			case R.id.menu_about_author:
+				Intent intent1 = new Intent(this, AboutAuthorActivity.class);
+				startActivity(intent1);
+				break;
+			case R.id.menu_action_settings:
+				Intent intent2 = new Intent(this, ActionSettingsActivity.class);
+				startActivity(intent2);
+				break;
+			case R.id.menu_theme_activity:
+				Intent intent3 = new Intent(this, ThemeActivity.class);
+				startActivity(intent3);
+				break;
+			default:
 
-        }
+		}
 
-        return super.onOptionsItemSelected(item);
-    }
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "start compass");
-        compass.start();
-    }
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(TAG, "start compass");
+		compass.start();
+	}
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        compass.stop();
-    }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		compass.stop();
+	}
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        compass.start();
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		compass.start();
+	}
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "stop compass");
-        compass.stop();
-    }
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(TAG, "stop compass");
+		compass.stop();
+	}
 
-    private void setupCompass() {
-        compass = new Compass(this);
-        Compass.CompassListener cl = getCompassListener();
-        compass.setListener(cl);
-    }
+	private void setupCompass() {
+		compass = new Compass(this);
+		Compass.CompassListener cl = getCompassListener();
+		compass.setListener(cl);
+	}
 
-    private void adjustArrow(float azimuth) {
-        Log.d(TAG, "установить вращение от " + currentAzimuth + " до "
-                + azimuth);
+	private void adjustArrow(float azimuth) {
+		Log.d(TAG, "установить вращение от " + currentAzimuth + " до "
+				+ azimuth);
 
-        Animation an = new RotateAnimation(-currentAzimuth, -azimuth,
-                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
-                0.5f);
-        currentAzimuth = azimuth;
+		Animation an = new RotateAnimation(-currentAzimuth, -azimuth,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
+		currentAzimuth = azimuth;
 
-        an.setDuration(500);
-        an.setRepeatCount(0);
-        an.setFillAfter(true);
+		an.setDuration(500);
+		an.setRepeatCount(0);
+		an.setFillAfter(true);
 
-        arrowView.startAnimation(an);
-    }
+		arrowView.startAnimation(an);
+	}
 
-    private void adjustSotwLabel(float azimuth) {
-        sotwLabel.setText(sotwFormatter.format(azimuth));
-    }
+	private void adjustSotwLabel(float azimuth) {
+		sotwLabel.setText(sotwFormatter.format(azimuth));
+	}
 
-    private Compass.CompassListener getCompassListener() {
-        return new Compass.CompassListener() {
-            @Override
-            public void onNewAzimuth(final float azimuth) {
-                // UI updates only in UI thread
-                // https://stackoverflow.com/q/11140285/444966
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        adjustArrow(azimuth);
-                        adjustSotwLabel(azimuth);
-                    }
-                });
-            }
-        };
-    }
+	private Compass.CompassListener getCompassListener() {
+		return new Compass.CompassListener() {
+			@Override
+			public void onNewAzimuth(final float azimuth) {
+				// UI updates only in UI thread
+				// https://stackoverflow.com/q/11140285/444966
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						adjustArrow(azimuth);
+						adjustSotwLabel(azimuth);
+					}
+				});
+			}
+		};
+	}
 }
