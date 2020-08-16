099aaf314c213210c4392f090a4beee9b5c350e2
vasia18 <studiovasia18@gmail.com>
2020-08-17 00:42:59 +0300

    Обновление 20200817_2

--- app/src/main/java/com/ivanov/compass/LogoActivity.java
+++ app/src/main/java/com/ivanov/compass/LogoActivity.java
@@ -1,4 +1,27 @@
 package com.ivanov.compass;
 
-public class LogoActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LogoActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_logo);
+
+		// Прописываем переход от заставки logoActivity к CompassActivity
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent i = new Intent(LogoActivity.this, CompassActivityOne.class);
+				startActivity(i);
+				finish();
+			}
+		}, 2*1000);
+
+
+	}
 }
