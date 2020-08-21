9f6dc0c86d33d09f69cbe10ce707e78842bafc0e
vasia18 <studiovasia18@gmail.com>
2020-08-21 10:53:19 +0300

    Обновление 20200820_5

--- app/src/main/java/com/ivanov/compass/CompassActivityOne.java
+++ app/src/main/java/com/ivanov/compass/CompassActivityOne.java
@@ -13,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -23,167 +24,176 @@ import com.google.android.material.navigation.NavigationView;
 import java.io.File;
 
 
-public class CompassActivityOne extends AppCompatActivity {
+public class CompassActivityOne extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final String TAG = "CompassActivity";
+	private static final String TAG = "CompassActivity";
 
-    private Compass compass;
-    private ImageView arrowView;
-    private TextView sotwLabel;  // SOTW is for "side of the world"
+	private Compass compass;
+	private ImageView arrowView;
+	private TextView sotwLabel;  // SOTW is for "side of the world"
 
-    private float currentAzimuth;
-    private SOTWFormatter sotwFormatter;
+	private float currentAzimuth;
+	private SOTWFormatter sotwFormatter;
 
-    DrawerLayout drawerLayout;      // Прописываем drawerLayout
-    NavigationView navigationView;  // Прописываем navigationView
-    Toolbar toolbar;                // Прописываем toolbar
+	DrawerLayout drawerLayout;      // Прописываем drawerLayout
+	NavigationView navigationView;  // Прописываем navigationView
+	Toolbar toolbar;                // Прописываем toolbar
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_compass);
+
+		sotwFormatter = new SOTWFormatter(this);
+
+		arrowView = findViewById(R.id.main_image_hands);
+		sotwLabel = findViewById(R.id.sotw_label);
+		setupCompass();
+
+		drawerLayout = findViewById(R.id.drawer_layout);  // Наодим по DI drawer_layout
+		drawerLayout = findViewById(R.id.nav_view);       // Находим по ID nav_view
+		drawerLayout = findViewById(R.id.toolbar);        // Находим по ID toolbar
+
+		/* -------------------------------     toolbar     --------------------------*/
+
+		setSupportActionBar(toolbar);
+		/* -------------------------------     navigationDrawerMenu     --------------------------*/
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		drawerLayout.addDrawerListener(toggle);
+		toggle.syncState();
+
+		navigationView.setNavigationItemSelectedListener(this);
+	}
+
+	private void setSupportActionBar(Toolbar toolbar) {
+	}
+
+
+	@Override
+	public void onBackPressed() {
+		if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+			drawerLayout.closeDrawer(GravityCompat.START);
+		} else {
+			super.onBackPressed();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_compass, menu);
+		return true;
+	}
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		int id = item.getItemId();
+
+		switch (id) {
+			case R.id.menu_about_app:
+				Intent intent = new Intent(this, AboutAppActivity.class);
+				startActivity(intent);
+				break;
+			case R.id.menu_about_author:
+				Intent intent1 = new Intent(this, AboutAuthorActivity.class);
+				startActivity(intent1);
+				break;
+			case R.id.menu_action_settings:
+				Intent intent2 = new Intent(this, ActionSettingsActivity.class);
+				startActivity(intent2);
+				break;
+			case R.id.menu_theme_activity:
+				Intent intent3 = new Intent(this, ThemeActivity.class);
+				startActivity(intent3);
+				break;
+			case R.id.share:
+				ApplicationInfo api = getApplicationContext().getApplicationInfo();
+				String apkpath = api.sourceDir;
+				Intent intent4 = new Intent(Intent.ACTION_SEND);
+				intent4.setType("Application/vnd.android.package-archive");
+				intent4.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(apkpath)));
+				startActivity(Intent.createChooser(intent4, "ShareVia"));
+				break;
+			default:
+
+
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(TAG, "start compass");
+		compass.start();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		compass.stop();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		compass.start();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(TAG, "stop compass");
+		compass.stop();
+	}
+
+	private void setupCompass() {
+		compass = new Compass(this);
+		Compass.CompassListener cl = getCompassListener();
+		compass.setListener(cl);
+	}
+
+	private void adjustArrow(float azimuth) {
+		Log.d(TAG, "установить вращение от " + currentAzimuth + " до "
+				+ azimuth);
+
+		Animation an = new RotateAnimation(-currentAzimuth, -azimuth,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
+		currentAzimuth = azimuth;
+
+		an.setDuration(500);
+		an.setRepeatCount(0);
+		an.setFillAfter(true);
+
+		arrowView.startAnimation(an);
+	}
+
+	private void adjustSotwLabel(float azimuth) {
+		sotwLabel.setText(sotwFormatter.format(azimuth));
+	}
+
+	private Compass.CompassListener getCompassListener() {
+		return new Compass.CompassListener() {
+			@Override
+			public void onNewAzimuth(final float azimuth) {
+				// UI updates only in UI thread
+				// https://stackoverflow.com/q/11140285/444966
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						adjustArrow(azimuth);
+						adjustSotwLabel(azimuth);
+					}
+				});
+			}
+		};
+	}
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_compass);
-
-        sotwFormatter = new SOTWFormatter(this);
-
-        arrowView = findViewById(R.id.main_image_hands);
-        sotwLabel = findViewById(R.id.sotw_label);
-        setupCompass();
-
-        drawerLayout = findViewById(R.id.drawer_layout);  // Наодим по DI drawer_layout
-        drawerLayout = findViewById(R.id.nav_view);       // Находим по ID nav_view
-        drawerLayout = findViewById(R.id.toolbar);        // Находим по ID toolbar
-
-        setSupportActionBar(toolbar);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-    }
-
-    private void setSupportActionBar(Toolbar toolbar) {
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_compass, menu);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return true;
-    }
-
-}
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.menu_about_app:
-                Intent intent = new Intent(this, AboutAppActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.menu_about_author:
-                Intent intent1 = new Intent(this, AboutAuthorActivity.class);
-                startActivity(intent1);
-                break;
-            case R.id.menu_action_settings:
-                Intent intent2 = new Intent(this, ActionSettingsActivity.class);
-                startActivity(intent2);
-                break;
-            case R.id.menu_theme_activity:
-                Intent intent3 = new Intent(this, ThemeActivity.class);
-                startActivity(intent3);
-                break;
-            case R.id.share:
-                ApplicationInfo api = getApplicationContext().getApplicationInfo();
-                String apkpath = api.sourceDir;
-                Intent intent4 = new Intent(Intent.ACTION_SEND);
-                intent4.setType("Application/vnd.android.package-archive");
-                intent4.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(apkpath)));
-                startActivity(Intent.createChooser(intent4, "ShareVia"));
-                break;
-            default:
-
-
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "start compass");
-        compass.start();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        compass.stop();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        compass.start();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "stop compass");
-        compass.stop();
-    }
-
-    private void setupCompass() {
-        compass = new Compass(this);
-        Compass.CompassListener cl = getCompassListener();
-        compass.setListener(cl);
-    }
-
-    private void adjustArrow(float azimuth) {
-        Log.d(TAG, "установить вращение от " + currentAzimuth + " до "
-                + azimuth);
-
-        Animation an = new RotateAnimation(-currentAzimuth, -azimuth,
-                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
-                0.5f);
-        currentAzimuth = azimuth;
-
-        an.setDuration(500);
-        an.setRepeatCount(0);
-        an.setFillAfter(true);
-
-        arrowView.startAnimation(an);
-    }
-
-    private void adjustSotwLabel(float azimuth) {
-        sotwLabel.setText(sotwFormatter.format(azimuth));
-    }
-
-    private Compass.CompassListener getCompassListener() {
-        return new Compass.CompassListener() {
-            @Override
-            public void onNewAzimuth(final float azimuth) {
-                // UI updates only in UI thread
-                // https://stackoverflow.com/q/11140285/444966
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        adjustArrow(azimuth);
-                        adjustSotwLabel(azimuth);
-                    }
-                });
-            }
-        };
     }
 }
